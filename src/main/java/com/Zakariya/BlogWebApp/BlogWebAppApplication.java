0a3b1b60b14d04908a6a8bf2efb73bea9a31f973
Zakariya Abdirizak <zakariyaabdirizak@outlook.com>
2024-04-19 18:54:25 +0100

    initial springboot setup using start.spring.io

---
+++ src/main/java/com/Zakariya/BlogWebApp/BlogWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.Zakariya.BlogWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Zakariya/BlogWebApp/BlogWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zakariya.BlogWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
